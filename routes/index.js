5ccb042e03ee4d7a5b3586c397db8af2e121eeb9
ameng <1508235075@qq.com>
2016-07-11 14:48:38 +0800

    Update index.js
    add album view

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,17 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 var Vac = require('./model');
-// Vac.remove({name:"tom"},function(err,doc){
-// 	if(err){
-// 		console.log(err);
-// 	}else{
-// 		console.log('delete ok');
-// 	}
-// });
-//console.log(Vac);
-// db.Vac.insert({"name":"user3","password":123456});
-var csstest = "<style>#testactive{background:#333}</style>";
-var csstest1 = "<style>#testactive1{background:#333}</style>";
-var csstest2 = "<style>#testactive2{background:#333}</style>";
-var css1 = "<style>.my-drop{diplay:none}</style>";
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
+
+var csstest = '<style>#testactive{background:#333}</style>';
+var csstest1 = '<style>#testactive1{background:#333}</style>';
+var csstest2 = '<style>#testactive2{background:#333}</style>';
+var css1 = '<style>.my-drop{diplay:none}</style>';
 function checkLog(req,res,next){
 	if(!req.session.name){
-		req.session.error = "请先登录！";
+		req.session.error = '请先登录！';
 		res.redirect('/login');
 	}
 	next();
@@ -29,12 +24,13 @@ function checkNotLog(req,res,next){
 	next();
 }
 router.get('/', function (req, res, next) {
-    Vac.find(function (err, data) {
-    	res.render('index', {
-            vacs: data,
-            css: csstest
-        });
-    });   
+  Vac.find(function (err, data) {
+  	res.render('index', {
+          vacs: data,
+          css: csstest
+      });
+  });
+
 });
 //login
 router.get('/login', function(req, res, next) {
@@ -51,14 +47,14 @@ router.post('/login',function(req, res, next){
 			console.log(err);
 		}else if(!data){
 			req.session.error = '用户名不存在';
-			res.redirect("/login");
+			res.redirect('/login');
 		}else{
 			if(password != data.password){ 	//查询到匹配用户名的信息，但相应的password属性不匹配
-				req.session.error = "密码错误";
-				res.redirect("/login");
+				req.session.error = '密码错误';
+				res.redirect('/login');
 			}else{ 									//信息匹配成功，则将此对象（匹配到的user) 赋给session.user  并返回成功
 				req.session.name = data.name;
-				res.redirect("/");
+				res.redirect('/');
 			}
 		}
 	});
@@ -79,10 +75,10 @@ router.post('/reg',function(req, res, next){
 			console.log(err);
 		}else if(password != repass){
 			req.session.error = '两次输入的密码不一样';
-			res.redirect("/reg");
+			res.redirect('/reg');
 		}else if(data){
 			req.session.error = '用户名已存在';
-			res.redirect("/reg");
+			res.redirect('/reg');
 		}else{
 			var doc = {name : req.body.name, password: req.body.password};
 			Vac.create(doc, function(error){
@@ -90,7 +86,7 @@ router.post('/reg',function(req, res, next){
 	        console.log(error);
 		    }else{
 	        console.log('save ok');
-	        res.redirect("/");
+	        res.redirect('/');
 		    }
 		  });
 		}
@@ -106,7 +102,7 @@ router.get('/logout',function(req, res, next){
 //article
 router.get('/article',checkLog);
 router.get('/article',function(req, res, next){
-  Vac.find({name:req.session.name,type:"article"},function (err, data) {
+  Vac.find({name:req.session.name,type:'article'},function (err, data) {
 	res.render('article', {
       vacs: data
     });
@@ -114,7 +110,7 @@ router.get('/article',function(req, res, next){
 })
 
 //addarticle
-// router.get('/addarticle',checkLog);
+router.get('/addarticle',checkLog);
 router.get('/addarticle',function(req, res, next){
 	res.render('addarticle');
 })
@@ -127,14 +123,14 @@ router.post('/addarticle',function(req, res, next){
 		}else{
 			var date = new Date();
 			var mon = date.getMonth() + 1;
-			var a = date.getFullYear() + "-" + mon + "-" + date.getDate();
-			var doc = {name:name, title:req.body.title, contant:req.body.contant,type:"article",year:a};
+			var a = date.getFullYear() + '-' + mon + '-' + date.getDate();
+			var doc = {name:name, title:req.body.title, contant:req.body.contant,type:'article',year:a};
 			Vac.create(doc, function(error){
 		    if(error){
 	        console.log(error);
 		    }else{
 	        console.log('save ok');
-	        res.redirect("/article");
+	        res.redirect('/article');
 		    }
 		  });
 		}
@@ -142,8 +138,23 @@ router.post('/addarticle',function(req, res, next){
 })
 
 //album
+router.get('/album',checkLog);
 router.get('/album',function(req, res, next){
-	res.render('album');
+	paths = [];
+	dir = '../public/img/'+req.session.name;
+  fs.readdirSync(dir).forEach(function (file) {
+    var pathname = path.join(dir, file);
+    if (fs.statSync(pathname).isDirectory()) {
+        console.log("wrong");
+    } else {
+			var reg = /.*public/;
+    	pathnames = pathname.replace(reg,"");
+    	paths.push(pathnames);
+    }
+  });
+	res.render('album',{
+     path:paths
+	});
 })
 router.post('/album',function(req, res, next){
 	var form = new formidable.IncomingForm();   //创建上传表单
@@ -187,6 +198,7 @@ router.post('/album',function(req, res, next){
     fs.renameSync(files.fulAvatar.path, newPath);  //重命名
   });
   res.locals.success = '上传成功';
-  res.render('album');
+  res.redirect('/album');
+  // res.redirect('/');
 })
 module.exports = router;
