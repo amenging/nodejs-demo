fac88cb8858e264e9a5abb122656974ce8fb10b7
amenging <1508235075@qq.com>
2018-01-24 00:48:11 -0800

    resolve problem: missing path, in app.js 3:14

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var logger = require('');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require("express-session");
